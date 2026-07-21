17a0cff3848084749364920621512846386bd758
fcbernardo <felipe.correa@mwonline.com.br>
2026-07-21 13:53:17 -0300

    Atualizando data de confirmação de presença

--- src/App.jsx
+++ src/App.jsx
@@ -88,7 +88,7 @@ export default function App() {
                       <path d="M20.84 4.61a5.5 5.5 0 0 0-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 0 0-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 0 0 0-7.78z"></path>
                     </svg>
                   </div>
-                  <span className="btn-label">Confirmar Presença</span>
+                  <span className="btn-label">Confirmar Presença até 22 de Agosto</span>
                 </a>
 
                 {/* Button 4: Access Site */}
